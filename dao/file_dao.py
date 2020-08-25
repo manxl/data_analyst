c57ab6d06c1a63d971fa07192bf351bf10f99d10
manxl <manxiaolei@sina.com>
2020-08-25 09:42:24 +0800

    My Study project!

--- dao/file_dao.py
+++ dao/file_dao.py
@@ -4,38 +4,36 @@ import datetime as dt
 
 p = os.path.dirname
 
-a = p(p(__file__)).__add__('test_data_k.csv')
 
+class CsvDAO:
+    @staticmethod
+    def read_test_data_from_csv():
+        # ,ts_code,trade_date
+        # ,open,high,low,
+        # close,pre_close,
+        # change,pct_chg,vol,amount
 
-def read_test_data_from_csv():
-    # ,ts_code,trade_date
-    # ,open,high,low,
-    # close,pre_close,
-    # change,pct_chg,vol,amount
+        file_name = p(p(__file__)).__add__('/data/test_data_k.csv')
+        return np.loadtxt(file_name, delimiter=',',
+                          usecols=(2, 3, 4, 5, 6),
+                          skiprows=3,
+                          unpack=True,
+                          dtype='M8[D], f8, f8, f8, f8',
+                          converters={2: lambda ymd: '-'.join([ymd.decode()[0:4], ymd.decode()[4:6], ymd.decode()[6:]])}
+                          )
 
-    file_name = p(p(__file__)).__add__('/data/test_data_k.csv')
-    return np.loadtxt(file_name, delimiter=',',
-                      usecols=(2, 3, 4, 5, 6),
-                      skiprows=3,
-                      unpack=True,
-                      dtype='M8[D], f8, f8, f8, f8',
-                      converters={2: lambda ymd: '-'.join([ymd.decode()[0:4], ymd.decode()[4:6], ymd.decode()[6:]])}
-                      )
+    @staticmethod
+    def read_test_data_from_csv_week_day():
+        # ,ts_code,trade_date
+        # ,open,high,low,
+        # close,pre_close,
+        # change,pct_chg,vol,amount
 
-
-def read_test_data_from_csv_week_day():
-    # ,ts_code,trade_date
-    # ,open,high,low,
-    # close,pre_close,
-    # change,pct_chg,vol,amount
-
-    file_name = p(p(__file__)).__add__('/data/test_data_k.csv')
-    return np.loadtxt(file_name, delimiter=',',
-                      usecols=(2, 3, 4, 5, 6),
-                      skiprows=3,
-                      unpack=True,
-                      dtype='f8, f8, f8, f8, f8',
-                      converters={2: lambda ymd: dt.datetime.strptime(ymd.decode(), '%Y%m%d').date().weekday()}
-                      )
-
-
+        file_name = p(p(__file__)).__add__('/data/test_data_k.csv')
+        return np.loadtxt(file_name, delimiter=',',
+                          usecols=(2, 3, 4, 5, 6),
+                          skiprows=3,
+                          unpack=True,
+                          dtype='f8, f8, f8, f8, f8',
+                          converters={2: lambda ymd: dt.datetime.strptime(ymd.decode(), '%Y%m%d').date().weekday()}
+                          )
--- np_sample/test_science.py
+++ np_sample/test_science.py
@@ -1,15 +1,153 @@
 import numpy as np
 import matplotlib.pyplot as mp
 import matplotlib.dates as md
-
+from dao.file_dao import CsvDAO as cd
 
 # from dao.file_dao import read_test_data_from_csv, read_test_data_from_csv_week_day
 
 
 class TestScience:
     @staticmethod
+    def test_init_np():
+        data = [('zs', [98, 70, 88], 16),
+                ('ls', [91, 71, 81], 15),
+                ('ww', [92, 72, 82], 17)]
+
+        # data.sort(key=lambda x: x[1][2])
+        print('=' * 32, 'method 1', '=' * 32)
+        ary = np.array(data, dtype='U2,3int32,int32')
+        print(ary, 'ww age:', ary[2]['f1'])
+
+        #
+        print('=' * 32, 'method 2', '=' * 32)
+        ary = np.array(data, dtype=[('name', 'str', 2),
+                                    ('score', 'int32', 3),
+                                    ('age', 'int32', 1)])
+        print(ary[1]['age'])
+
+        #
+        print('=' * 32, 'way 3', '=' * 32)
+        ary = np.array(data, dtype={'names': ['name', 'scores', 'age'], 'formats': ['U2', '3int32', 'int32']})
+        print('way 3 ', ary)
+
+        print('=' * 32, 'way 4', '=' * 32)
+        d = np.array(data, dtype={'name': ('U3', 0), 'scores': ('3int32', 16), 'age': ('int32', 32)})
+        print(d[0]['name'])
+
+        print('=' * 32, 'way 4', '=' * 32)
+        d = np.array
+
+    @staticmethod
+    def init_datetime64():
+        data = ['2011', '2011-01-02', '2012-01-01', '2012-02-01', '2012-02-01 10:11:12']
+
+        print('=' * 32, 'origin', '=' * 32)
+        dates = np.array(data)
+        print(dates, dates.dtype)
+
+        print('=' * 32, 'M8[D]', '=' * 32)
+        dates = dates.astype('M8[M]')
+        print(dates, dates.dtype)
+        print(dates[2] - dates[0])
+
+    @staticmethod
+    def test_d():
+        a = np.arange(1, 9)
+        print("a", a)
+        b = a.reshape(2, 4)
+        print('b', b)
+        c = b.reshape(2, 2, 2)
+        print('c', c)
+        d = c.ravel()
+        print('d', d)
+
+        print('=' * 32, 'view reshape')
+        a = np.arange(1, 10)
+        print(a, a.shape)
+        b = a.reshape(3, 3)
+        print(b, b.reshape)
+        b[0, 0] = 999
+        print('a:', a)
+        print('b:', b)
+
+        print('=' * 32, 'copy reshape')
+        d = b.flatten()
+        # d = b.ravel()
+        print('d(1):', d)
+        b[0, 0] = 333
+        print('d(1):', d)
+
+        d.shape = (3, 3)
+        print(d)
+        d.resize((9,))
+        print(d)
+
+    @staticmethod
+    def test_slice():
+        print('=' * 32, '1d')
+        a = np.arange(1, 10)
+        print(a[1:4:1])
+        print(a[4:-1:1])
+        print(a[4:1:1])
+        print(a[-1:-4:-1])
+        print(a[-1:-4:1])
+
+        print('=' * 32, '3d')
+        a = np.arange(1, 28)
+        a.resize(3, 3, 3)
+        print(a)
+        print('=', 1)
+        print(a[1, :, :])
+        print('=', 2)
+        print(a[:, 1, :])
+        print('=', 3)
+        print(a[:, :, 1])
+
+        print(a[:, :, 1])
+
+    @staticmethod
+    def test_d():
+        a = np.arange(0, 8).reshape(2, 4)
+        b = np.arange(8, 16).reshape(2, 4)
+        c = np.arange(16, 24).reshape(2, 4)
+        d = np.arange(24, 32).reshape(2, 4)
+
+        ap = np.dstack((a, b))
+        bp = np.dstack((a, b))
+
+        print('=' * 32, 'ap:\n')
+        print(ap)
+        print('=' * 32, 'bp:\n')
+        print(bp)
+
+        print('=' * 48, 'aa:\n')
+        o = np.dstack((a, b, c, d))
+        print(o)
+        print('')
+        print(np.dsplit(o, 2))
+
+        print('o.shape:', o.shape)
+        print('o.dtype:', o.dtype)
+        print('o.ndim:', o.ndim)
+        print('o.size:', o.size)
+        print('o.itemsize:', o.itemsize)
+        print('o.nbytes:', o.nbytes)
+        print('o.real:\n', o.real)
+        print('for:\n', [e for e in a.flat])
+        print('o.tolist:\n', o.tolist())
+
+    @staticmethod
+    def test_np_a():
+        a = np.ones(5)
+        b = a * 2
+        r = np.vstack((a, b))
+        print(r)
+        b = r[:, 1] / 5
+        print(b)
+
+    @staticmethod
     def func_test():
-        d_date, d_open, d_high, d_low, d_close = read_test_data_from_csv()
+        d_date, d_open, d_high, d_low, d_close = cd.read_test_data_from_csv()
         std = np.std(d_close)
         print('std 0:{}'.format(std))
         mean = d_close.mean()
@@ -26,7 +164,7 @@ class TestScience:
 
     @staticmethod
     def test_ave():
-        d_date, d_open, d_high, d_low, d_close = read_test_data_from_csv_week_day()
+        d_date, d_open, d_high, d_low, d_close = cd.read_test_data_from_csv_week_day()
         avg = np.zeros(5)
         for w in range(avg.size):
             avg[w] = np.mean(d_close[d_date == w])
@@ -58,7 +196,7 @@ class TestScience:
 
     @staticmethod
     def test_super_k():
-        d_date, d_open, d_high, d_low, d_close = read_test_data_from_csv()
+        d_date, d_open, d_high, d_low, d_close = cd.read_test_data_from_csv()
 
         # mp.figure('New 000001.SZ K', facecolor='lightgray')
         mp.figure('New 000001.SZ K')
@@ -118,7 +256,7 @@ class TestScience:
 
     @staticmethod
     def test_will():
-        d_date, d_open, d_high, d_low, d_close = read_test_data_from_csv()
+        d_date, d_open, d_high, d_low, d_close = cd.read_test_data_from_csv()
         n = 3
         a = np.zeros((n, n))
         for i in range(n):
@@ -134,7 +272,7 @@ class TestScience:
 
     @staticmethod
     def test_base_science():
-        d_date, d_open, d_high, d_low, d_close = read_test_data_from_csv()
+        d_date, d_open, d_high, d_low, d_close = cd.read_test_data_from_csv()
         #  方差计算
         d_open = d_open[:5]
 
@@ -164,7 +302,7 @@ class TestScience:
 
     @staticmethod
     def test_smooth():
-        d_date, d_open, d_high, d_low, d_close = read_test_data_from_csv()
+        d_date, d_open, d_high, d_low, d_close = cd.read_test_data_from_csv()
         d = d_date[:]
         n = 7
         profit_a = np.diff(d_open)
@@ -220,7 +358,7 @@ class TestScience:
 
     @staticmethod
     def test_methods():
-        d_date, d_open, d_high, d_low, d_close = read_test_data_from_csv()
+        d_date, d_open, d_high, d_low, d_close = cd.read_test_data_from_csv()
         diff_open = np.diff(d_open)
         mp.figure('figure test methods ')
         mp.title('title test methods ')
@@ -439,17 +577,17 @@ class TestScience:
 
 
 if __name__ == '__main__':
-    # TestScience.func_test()
-    # TestScience.test_apply_along_axis()
-    # TestScience.test_super_slice_array()
-    # TestScience.test_super_k()
-    # TestScience.test_will()
-    # TestScience.test_base_science()
-    # TestScience.test_smooth()
-    # TestScience.test_methods()
-    # TestScience.test_matrix()
-    # TestScience.test_array()
-    # TestScience.test_random()
-    # TestScience.test_interp()
-    # TestScience.test_sci()
+    TestScience.func_test()
+    TestScience.test_apply_along_axis()
+    TestScience.test_super_slice_array()
+    TestScience.test_super_k()
+    TestScience.test_will()
+    TestScience.test_base_science()
+    TestScience.test_smooth()
+    TestScience.test_methods()
+    TestScience.test_matrix()
+    TestScience.test_array()
+    TestScience.test_random()
+    TestScience.test_interp1d()
+    TestScience.test_sci()
     TestScience.test_fin()
