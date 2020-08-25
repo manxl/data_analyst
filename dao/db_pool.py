28946a22cdac947ba6bc61fb48c07781af5c4dde
manxl <manxiaolei@sina.com>
2020-08-25 18:51:53 +0800

    My Study project!

---
+++ dao/db_pool.py
@@ -0,0 +1,216 @@
+from DBUtils.PooledDB import PooledDB, SharedDBConnection
+from DBUtils.PersistentDB import PersistentDB
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+import yaml, os, re, pymysql, time, xlwt, sys, hashlib, sqlalchemy
+from time import sleep, time
+from sqlalchemy import create_engine
+
+
+class MySQL:
+    __pool = None
+
+    @staticmethod
+    def connect():
+        return __class__.get_connection()
+
+    @staticmethod
+    def get_connection():
+        # 构造函数，创建数据库连接、游标
+        if __class__.__pool is None:
+            __class__.__pool = PooledDB(
+                pymysql,
+                # sqlalchemy,
+                10,
+                host='127.0.0.1',
+                user='manxl',
+                port=3306,
+                passwd='111',
+                db='analyst',
+                # use_unicode=True，
+                charset='utf8')
+        # return __class__.__pool.connection()
+        return __class__.__pool.connection()
+
+    def get_all(self, sql, param=None):
+        """
+        @summary: 执行查询，并取出所有结果集
+        @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
+        @param param: 可选参数，条件列表值（元组/列表）
+        @return: result list(字典对象)/boolean 查询到的结果集
+        """
+        con = __class__.get_connection()
+        cur = con.cursor(pymysql.cursors.DictCursor)
+        try:
+            if param is None:
+                count = cur.execute(sql)
+            else:
+                count = cur.execute(sql, param)
+            if count > 0:
+                result = cur.fetchall()
+            else:
+                result = False
+        except Exception as e:
+            # con.rollback()  # 事务回滚
+            print('SQL执行有误,原因:', e)
+            result = None
+        finally:
+            cur.close()
+            con.close()
+
+        return result
+
+    def get_one(self, sql, param=None):
+        con = self.__pool.connection()
+        cur = con.cursor(pymysql.cursors.DictCursor)
+        try:
+            if param is None:
+                count = cur.execute(sql)
+            else:
+                count = cur.execute(sql, param)
+            if count > 0:
+                result = cur.fetchone()
+            else:
+                result = False
+        except Exception as e:
+            # con.rollback()  # 事务回滚
+            print('SQL执行有误,原因:', e)
+            result = None
+        finally:
+            cur.close()
+            con.close()
+        return result
+
+    def getMany(self, sql, num, param=None):
+        """
+        @summary: 执行查询，并取出num条结果
+        @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
+        @param num:取得的结果条数
+        @param param: 可选参数，条件列表值（元组/列表）
+        @return: result list/boolean 查询到的结果集
+        """
+        if param is None:
+            count = cur.execute(sql)
+        else:
+            count = cur.execute(sql, param)
+        if count > 0:
+            result = cur.fetchmany(num)
+        else:
+            result = False
+        return result
+
+    def insertOne(self, sql, value):
+        """
+        @summary: 向数据表插入一条记录
+        @param sql:要插入的ＳＱＬ格式
+        @param value:要插入的记录数据tuple/list
+        @return: insertId 受影响的行数
+        """
+        cur.execute(sql, value)
+        return self.__getInsertId()
+
+    def insertMany(self, sql, values):
+        """
+        @summary: 向数据表插入多条记录
+        @param sql:要插入的ＳＱＬ格式
+        @param values:要插入的记录数据tuple(tuple)/list[list]
+        @return: count 受影响的行数
+        """
+        count = cur.executemany(sql, values)
+        return count
+
+    def __getInsertId(self):
+        """
+        获取当前连接最后一次插入操作生成的id,如果没有则为０
+        """
+        self.__cur.execute("SELECT @@IDENTITY AS id")
+        result = cur.fetchall()
+        return result[0]['id']
+
+    def __query(self, sql, param=None):
+        if param is None:
+            count = cur.execute(sql)
+        else:
+            count = cur.execute(sql, param)
+        return count
+
+    def update(self, sql, param=None):
+        """
+        @summary: 更新数据表记录
+        @param sql: ＳＱＬ格式及条件，使用(%s,%s)
+        @param param: 要更新的  值 tuple/list
+        @return: count 受影响的行数
+        """
+        return self.__query(sql, param)
+
+    def delete(self, sql, param=None):
+        """
+        @summary: 删除数据表记录
+        @param sql: ＳＱＬ格式及条件，使用(%s,%s)
+        @param param: 要删除的条件 值 tuple/list
+        @return: count 受影响的行数
+        """
+        return self.__query(sql, param)
+
+    def begin(self):
+        """
+        @summary: 开启事务
+        """
+        self.__pool.autocommit(0)
+
+    def end(self, option='commit'):
+        """
+        @summary: 结束事务
+        """
+        if option == 'commit':
+            self.__pool.commit()
+        else:
+            self.__pool.rollback()
+
+    def dispose(self, flag):
+        if flag == 1:
+            self.end('commit')
+        elif flag == 0:
+            self.end('rollback')
+        self.__pool.close()
+
+
+def func(pool):
+    print('start')
+    sql = 'select count(*) from  zun_flush.act_activity_notice	where LENGTH(creater) = %s;'
+    a = pool.get_all(sql, param=19)
+    a = a[:]
+    for i in a:
+        print(a)
+    # sleep(2)
+    pool.dispose(-1)
+    print('1111')
+    return 'ok'
+
+
+if __name__ == '__main__':
+    start = time()
+    pool = MySQL()
+    threadPool = ThreadPoolExecutor(max_workers=5, thread_name_prefix="test_")
+    fs = []
+    for i in range(1, 10):
+        f = threadPool.submit(func, pool)
+        fs.append(f)
+
+    for f in fs:
+        print(f.result())
+    print('main waiting')
+    threadPool.shutdown(wait=True)
+    print('main over:{}'.format(time() - start))
+    sleep(10)
+    print('main out')
+
+    # for i in a:
+    #     print(i)
+    # pool.begin()
+    #         sql = 'update test.test_user set name= %s where id = %s'
+    #         a = pool.update(sql, ('aa1', 1))
+    #         b = pool.update(sql, ('bb1', 2))
+    #         print(a, '-' * 32, b)
+    #         pool.end('commit')
+    # pool.dispose(isEnd=1)
--- stock/tushare_dao.py
+++ stock/tushare_dao.py
@@ -1,11 +1,15 @@
 import datetime
-
+import pandas as pd
+import numpy as np
 import tushare as ts
+from sqlalchemy.types import VARCHAR, DATE, INT, Integer
 from sqlalchemy import create_engine
+
+db_name = 'analyst'
 
 
 def get_engine():
-    return create_engine("mysql+pymysql://manxl:111@localhost:3306/analyse?charset=utf8")
+    return create_engine("mysql+pymysql://manxl:111@127.0.0.1:3306/{}?charset=utf8".format(db_name), pool_size=20)
 
 
 pro = ts.pro_api()
@@ -15,11 +19,6 @@ class TuShareDao:
 
     @staticmethod
     def get_stock_list_all():
-        """
-        is_hs	    str	N	是否沪深港通标的，N否 H沪股通 S深股通
-        list_status	str	N	上市状态： L上市 D退市 P暂停上市，默认L
-        exchange	str	N	交易所 SSE上交所 SZSE深交所 HKEX港交所(未上线)
-        """
         fileds = 'ts_code,symbol,name,area,industry,fullname,enname,market,exchange,curr_type,list_status,list_date,delist_date,is_hs'
         d_l = pro.stock_basic(exchange='', list_status='L', fields=fileds)
         print('L', len(d_l))
@@ -29,7 +28,7 @@ class TuShareDao:
         print('P', len(d_p))
         data = d_l.append(d_d).append(d_p)
         print('all size:', len(data))
-        data.to_sql('stock_list', get_engine(), schema='analyse')
+        data.to_sql('stock_list', get_engine(), schema=db_name)
 
     @staticmethod
     def get_stock_cf():
@@ -47,8 +46,60 @@ class TuShareDao:
 
     @staticmethod
     def get_trade_dates():
-        df = pro.query('trade_cal', start_date='20180101', end_date='20181231')
-        df.to_sql('trade_date', get_engine(), schema='analyse')
+        template_start = '{}00101'
+        template_end = '{}91231'
+        data = None
+        for i in range(4):
+            print(i)
+            t = 199 + i
+            start, end = template_start.format(t), template_end.format(t)
+            df = pro.query('trade_cal', start_date=start, end_date=end)
+            if data is not None:
+                data = data.append(df, ignore_index=True)
+            else:
+                data = df
+            print('start:{},date:{}'.format(start, len(data)))
+
+        # data.to_sql('trade_date_o', get_engine(), if_exists='replace', schema=db_name)
+        df = data
+        df['y'] = df['cal_date'].apply(lambda s: int(s[:4]))
+        df['m'] = df['cal_date'].apply(lambda s: int(s[4:6]))
+        # df['cal_date'] = df['cal_date'].astype('M8[D]')
+        # df.reset_index(drop=True)
+
+        df.set_index(['y', 'm', 'cal_date'])
+        df = df[df['is_open'] == 1]
+        df = df.reindex(columns=['y', 'm', 'cal_date', 'is_open', 'exchange'])
+        df.to_sql('trade_date_detail',
+                  get_engine(),
+                  index=False,
+                  dtype={'cal_date': DATE(), 'y': Integer(), 'm': INT(), 'is_open': INT(), 'exchange': VARCHAR(8)},
+                  # dtype={'cal_date': 'M8[d]'},
+                  if_exists='replace', schema=db_name)
+        '''
+        分组插入扩展表
+        '''
+        grouped_m = df.groupby(['y', 'm'])
+        # for a, g in grouped_m:
+        #     print(a)
+        #     print(g)
+        r1 = grouped_m['cal_date'].agg([np.min, np.max])
+        r1 = r1.rename(columns={'amin': 'first', 'amax': 'last'})
+        r1['y'] = pd.Series(r1.index.get_level_values('y'), index=r1.index)
+        r1['m'] = pd.Series(r1.index.get_level_values('m'), index=r1.index)
+
+        grouped_m = df.groupby(['y'])
+        r2 = grouped_m['cal_date'].agg([np.min, np.max])
+        r2 = r2.rename(columns={'amin': 'first', 'amax': 'last'})
+        r2['y'] = pd.Series(r2.index.get_level_values('y'), index=r2.index)
+        r2['m'] = pd.Series(np.zeros(len(r2)), index=r2.index)
+
+        r = r1.append(r2, ignore_index=True)
+        r = r.reindex(columns=['y', 'm', 'first', 'last'])
+        r.to_sql('trade_date', get_engine(),
+                 index=False,
+                 dtype={'start': DATE(), 'end': DATE(), 'y': Integer(), 'm': INT()}
+                 , if_exists='replace', schema=db_name)
 
     @staticmethod
     def get_divid():
@@ -56,11 +107,11 @@ class TuShareDao:
         # df = pro.dividend(ts_code='600036.SH')
         df = pro.dividend(ts_code='002230.SZ')
         print(df)
-        df.to_sql('stock_divide', get_engine(), schema='analyse', if_exists='append')
+        df.to_sql('stock_divide', get_engine(), schema=db_name, if_exists='append')
 
 
 if __name__ == '__main__':
     # TuShareDao.get_stock_cf()
     # TuShareDao.get_stock_list_all()
-    # TuShareDao.get_trade_dates()
-    TuShareDao.get_divid()
+    TuShareDao.get_trade_dates()
+    # TuShareDao.get_divid()
