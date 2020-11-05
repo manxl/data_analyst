df1a977be8d72ac6321f3e952e6e1f9bd2d89f0d
manxl <manxiaolei@sina.com>
2020-11-05 23:52:27 +0800

    commit

--- web/web.py
+++ web/web.py
@@ -12,10 +12,17 @@ LOG_FROMAT = "%(asctime)s\t%(filename)s\t[line:%(lineno)d]\t%(levelname)s\t%(mes
 DATE_FORMAT = "%a,%d %b %Y-%m-%d %H:%M:%S %p"
 logging.basicConfig(level=logging.DEBUG, format=LOG_FROMAT, datefmt=DATE_FORMAT, filemode='w')
 
-
 app = Flask(__name__)
 
+# https://www.w3cschool.cn/flask/flask_url_building.html
+@app.route('/hello/<int:age>/<float:price>/')
+def hello(age, price):
+    return 'age is {} \t price is {}'.format(age, price)
 
+
+@app.route('/error/')
+def error():
+    return 'aaa'
 
 
 @app.route('/')
@@ -26,17 +33,17 @@ def root():
 @app.route('/meta/reload_income')
 def reload_income():
     force = request.args['force']
-    income_dao.process()
+    # income_dao.process()
     return render_template('main.html')
 
 
 @app.route('/ml')
 def test_graph122():
-    df = pd.read_sql_query("select * from stock_balancesheet where ts_code = '601318.SH' and m = 12;", pool.get_engine())
+    df = pd.read_sql_query("select * from stock_balancesheet where ts_code = '000001.SZ' and m = 12;", pool.get_engine())
 
     mp.plot(df['end_date'], df['total_cur_assets'], label='123')
-    # mp.savefig('D:/storage/workspaces/idea-2020/data_analyst/web/static/images/new_plot.png')
-    return render_template('abc.html', name=df, url='static/images/new_plot.png')
+    mp.savefig('D:/storage/workspaces/idea-2020/data_analyst/web/static/images/new_plot1.png')
+    return render_template('abc.html', name=df, url='static/images/new_plot1.png')
 
 
 if __name__ == '__main__':
